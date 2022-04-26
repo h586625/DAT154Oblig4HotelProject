d6c7c288dae016ca18f82c2961ccf211cb71464b
h586625 <586625@stud.hvl.no>
2022-04-26 20:35:25 +0200

    Add .gitignore for VS

--- FrontDesk/obj/Debug/net6.0-windows/App.g.i.cs
+++ FrontDesk/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace HotelFrontDeskApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace HotelFrontDeskApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             HotelFrontDeskApp.App app = new HotelFrontDeskApp.App();
             app.InitializeComponent();
