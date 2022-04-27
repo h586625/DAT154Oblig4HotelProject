8f9f38d08d53086440f021d2688f8fdf2cc9a862
h586625 <586625@stud.hvl.no>
2022-04-27 21:01:57 +0200

    Blabla

---
+++ Library/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Reservations = new HashSet<Reservation>();
+        }
+
+        [Key]
+        public int Usernr { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
