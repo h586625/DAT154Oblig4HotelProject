3f36ceb52dcc74464407acc28cee2e6298a805d2
PaTrond <trond@hauge.app>
2022-04-27 11:45:50 +0200

    Mobile app init

---
+++ RoomServices/RoomServices/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RoomServices.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RoomServices.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ RoomServices/RoomServices/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RoomServices.Models;
+using RoomServices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoomServices.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
