f9652dee661555195c305656aaa8bcdf8d569b72
h586625 <586625@stud.hvl.no>
2022-05-02 00:12:29 +0200

    Set up working Blazor app with dbContext

--- CustomerBooking/Program.cs
+++ CustomerBooking/Program.cs
@@ -1,16 +1,32 @@
-using CustomerBooking;
+using CustomerBooking.Data;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyModel;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContextFactory<ContactContext>(opt =>
-//    opt.UseSqlite($"Data Source={nameof(ContactContext.ContactsDb)}.db"));
+// Add services to the container.
+builder.Services.AddDbContext<Library.Data.dat154_2022_42Context>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var app = builder.Build();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-await builder.Build().RunAsync();
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
