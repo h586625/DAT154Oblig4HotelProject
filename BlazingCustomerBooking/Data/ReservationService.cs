92bfed4e375bb7f840f84f40c535ed8b5a9b3620
h586616 <lottemsteen@gmail.com>
2022-05-03 16:56:19 +0200

    New project

---
+++ BlazingCustomerBooking/Data/ReservationService.cs
@@ -0,0 +1,49 @@
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazingCustomerBooking.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationService : ControllerBase
+    {
+        private dat154_2022_42Context _ctx;
+        public ReservationService(dat154_2022_42Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public Task<Reservation[]?> GetAllReservations()
+        {
+            return Task.FromResult(_ctx?.Reservations?.ToArray());
+        }
+
+        [HttpGet("AvailableRooms")]
+        public Task<Room[]?> GetAvailableRooms()
+        {
+            // TODO: Move this data into the function parameters
+            int beds = 1;
+            DateTime start = DateTime.Today;
+            DateTime end = DateTime.Now;
+
+            // This returned errors
+            //return Task.FromResult(Library.Controller.GetAvailableRooms(_ctx, 1, 1, DateTime.Now, DateTime.Now)?.ToArray());
+
+            // This can certainly be improved, but is a decent startingpoint.
+            return Task.FromResult(_ctx?.Rooms?
+                .Where(room => room.Beds == beds)
+                .Where(room => !room.Reservations
+                    .Any(reservation => reservation.DateStart >= start && reservation.DateStart < end // If there is a reserved start date that's in between the requested start and end date.
+                    || reservation.DateStart < start && reservation.DateEnd > start // If there is a reservation that starts before the start date but ends after the start date.
+                    || reservation.DateStart < end && reservation.DateEnd >= end)) // If there is a reservation that starts before the end date but ends after the end date.
+                .ToArray());
+        }
+
+        /*public Task<Reservation> CreateReservation(Room? room)
+        {
+           // TODO: Add reservation logic given a room (might need more parameters to make a full reservation?
+        }*/
+    }
+}
---
+++ BlazingCustomerBooking/Program.cs
@@ -0,0 +1,44 @@
+using BlazingCustomerBooking.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<Library.Data.dat154_2022_42Context>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ReservationService>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
