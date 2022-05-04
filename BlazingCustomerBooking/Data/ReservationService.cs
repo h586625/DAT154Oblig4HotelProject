8228d81563702052fc418b1bb6982406e73f9875
h586616 <lottemsteen@gmail.com>
2022-05-04 16:03:00 +0200

    hmm

--- BlazingCustomerBooking/Data/ReservationService.cs
+++ BlazingCustomerBooking/Data/ReservationService.cs
@@ -21,19 +21,18 @@ namespace BlazingCustomerBooking.Data
         }
 
         [HttpGet("AvailableRooms")]
-        public Task<Room[]?> GetAvailableRooms()
+        public Task<Room[]?> GetAvailableRooms(DateTime start, DateTime end)
         {
             // TODO: Move this data into the function parameters
-            int beds = 1;
-            DateTime start = DateTime.Today;
-            DateTime end = DateTime.Now;
+          
+           // DateTime start = DateTime.Today;
+           // DateTime end = DateTime.Now;
 
             // This returned errors
             //return Task.FromResult(Library.Controller.GetAvailableRooms(_ctx, 1, 1, DateTime.Now, DateTime.Now)?.ToArray());
 
             // This can certainly be improved, but is a decent startingpoint.
             return Task.FromResult(_ctx?.Rooms?
-                .Where(room => room.Beds == beds)
                 .Where(room => !room.Reservations
                     .Any(reservation => reservation.DateStart >= start && reservation.DateStart < end // If there is a reserved start date that's in between the requested start and end date.
                     || reservation.DateStart < start && reservation.DateEnd > start // If there is a reservation that starts before the start date but ends after the start date.
@@ -41,9 +40,10 @@ namespace BlazingCustomerBooking.Data
                 .ToArray());
         }
 
-        /*public Task<Reservation> CreateReservation(Room? room)
+        public Task<Reservation> CreateReservation(Room? room)
         {
+            return 
            // TODO: Add reservation logic given a room (might need more parameters to make a full reservation?
-        }*/
+        }
     }
 }
