fed639eac2f2119c2f9c98523b7a64177a8571ca
h586616 <lottemsteen@gmail.com>
2022-05-05 12:31:20 +0200

    Added login

---
+++ BlazingCustomerBooking/Auth/BlazorAuthenticationStateProvider.cs
@@ -0,0 +1,62 @@
+﻿using Library.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazingCustomerBooking.Auth
+{
+    public class BlazorAuthenticationStateProvider : AuthenticationStateProvider, IHostEnvironmentAuthenticationStateProvider
+    {
+        private dat154_2022_42Context _ctx;
+
+        private ClaimsPrincipal AnonymousUser => new(new ClaimsIdentity(Array.Empty<Claim>()));
+        private Task<AuthenticationState> _authenticationStateTask;
+
+        public BlazorAuthenticationStateProvider(dat154_2022_42Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (_authenticationStateTask == null)
+                return await Task.FromResult(new AuthenticationState(AnonymousUser));
+
+            return await _authenticationStateTask;
+        }
+
+        public void SetAuthenticationState(Task<AuthenticationState> authenticationStateTask)
+        {
+            _authenticationStateTask = authenticationStateTask ?? throw new ArgumentNullException(nameof(authenticationStateTask));
+            NotifyAuthenticationStateChanged(_authenticationStateTask);
+        }
+
+        private ClaimsPrincipal AsClaimsPrincipale(Library.Models.User user)
+        {
+            var claims = new[] {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, "user"),
+                new Claim("known_user", "known_user"),
+            };
+            var identity = new ClaimsIdentity(claims, "faked");
+            return new ClaimsPrincipal(identity);
+        }
+
+        public bool SignIn(Library.Models.User user)
+        {
+            if (_ctx?.Users?.FirstOrDefault(x => x.Name == user.Name && x.Password == user.Password) != null)
+            {
+                var result = Task.FromResult(new AuthenticationState(AsClaimsPrincipale(user)));
+                SetAuthenticationState(result);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SignOut()
+        {
+            var result = Task.FromResult(new AuthenticationState(AnonymousUser));
+            SetAuthenticationState(result);
+        }
+    }
+}
--- BlazingCustomerBooking/Program.cs
+++ BlazingCustomerBooking/Program.cs
@@ -1,17 +1,37 @@
+using BlazingCustomerBooking.Auth;
 using BlazingCustomerBooking.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<Library.Data.dat154_2022_42Context>();
+builder.Services.AddScoped<BlazorAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<BlazorAuthenticationStateProvider>());
+builder.Services.AddAuthorizationCore();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<ReservationService>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("KnownUsersOnly", policy => policy.RequireClaim("known_user"));
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
+//.AddCookie();
 
 var app = builder.Build();
 
@@ -31,6 +51,9 @@ else
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseRouting();
 
